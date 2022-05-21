12f171e2abb86a4b33139ac3a8c69c042a121b9a
Gabriel Dittrick <32310761+gabides@users.noreply.github.com>
2022-05-22 00:46:01 +0200

    feat(demo): Go to step 3

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -30,6 +30,18 @@
           <ion-row  v-if="this.isCameraOn" class="ion-align-self-center ion-justify-content-center">
             <ion-button :onclick="stopCamera" color="primary">Stop Camera</ion-button>
           </ion-row>  
+          <ion-row  v-if="this.isCameraOn" style="margin-top: 80%;">
+            <ion-card v-if="this.isCameraOn" style="width: 100%;">
+              <ion-card-header>
+                <ion-card-title>Detection</ion-card-title>
+              </ion-card-header>
+              <ion-card-content>
+                <div>Class: {{ this.detectedClass }}</div>
+                <div>Score: {{ this.detectionConfidence.toFixed(0) }}%</div>
+                <div>Time: {{ this.inferenceTime.toFixed(0) }}ms</div>
+              </ion-card-content>
+            </ion-card>
+          </ion-row>  
         </ion-col>
     </ion-grid>
     </ion-content>
@@ -39,8 +51,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonGrid, IonCol, IonRow, IonButton } from '@ionic/vue';
-import { CameraPreview, CameraPreviewOptions } from '@capacitor-community/camera-preview';
+import { CameraPreview, CameraPreviewOptions, CameraSampleOptions } from '@capacitor-community/camera-preview';
+import { classifyImage, initializeModel, INFERENCE_IMAGE_SIZE } from '@/services/image-classification.service';
 
+
+initializeModel()
 
 export default defineComponent({
   name: 'Tab1Page',
@@ -65,11 +80,40 @@ export default defineComponent({
       };
       await CameraPreview.start(cameraPreviewOptions);
       this.isCameraOn = true;
+      await this.detect();
     },
     async stopCamera() {
       await CameraPreview.stop();
       this.isCameraOn = false
     },
+    async detect() {
+      if (!this.isCameraOn) {
+        return;
+      } else {
+        const cameraSampleOptions: CameraSampleOptions = {
+          quality: 50
+        };
+        const result = await CameraPreview.captureSample(cameraSampleOptions);
+        const base64PictureData = "data:image/png;base64," + result.value;
+        let imageElement = await this.getImageFromBas64(base64PictureData);
+        const [classes, time] = await classifyImage(imageElement);
+        this.inferenceTime = time;
+        this.detectedClass = classes[0].className;
+        this.detectionConfidence = classes[0].probability;
+        await this.detect();
+      }
+    },
+    getImageFromBas64(base64: string): Promise<HTMLImageElement> {
+      return new Promise((resolve) => {
+        const img: HTMLImageElement = document.createElement('img');
+        img.addEventListener('load', () => {
+          img.width = INFERENCE_IMAGE_SIZE;
+          img.height = INFERENCE_IMAGE_SIZE;
+          resolve(img);
+        });
+        img.src = base64;
+      });
+    }
   }
 });
 </script>
