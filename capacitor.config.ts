bdfa77718e6b425c96c3b1f45506c44dc6967103
Gabriel Dittrick <32310761+gabides@users.noreply.github.com>
2022-05-21 20:15:33 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-tfjs-classifier',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
